d1b374f00a13abe16aac5bf6e00aecec2a7199cc
VexoLi <joanvicens@paucasesnovescifp.cat>
2025-01-24 20:28:22 +0100

    primera version ejecutable

--- backend/whatsapp_api/main.py
+++ backend/whatsapp_api/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from backend.app.routes import users
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
